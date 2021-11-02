b9370374b6514bfaa4897d42893e48ecc2110587
Guilherme Assis <guilherme660@hotmail.com>
2021-11-01 22:49:51 -0300

    Criando Estrutura do Porjeto Inicial

---
+++ backend/dsvendas/src/test/java/io/guilhermefasilva/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package io.guilhermefasilva.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
