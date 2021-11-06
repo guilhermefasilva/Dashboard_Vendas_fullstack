6b97a187856e92b2c47352b7b946f1979ea222be
Guilherme Assis <guilherme660@hotmail.com>
2021-11-06 17:04:19 -0300

    BarChart integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,4 +1,10 @@
 export type SaleSum ={
-  salleName: string,
-  sum: number
+  salleName: string;
+  sum: number;
+}
+
+export type SaleSuccess ={
+  salleName: string;
+  visited: number;
+  deals: number;
 }
